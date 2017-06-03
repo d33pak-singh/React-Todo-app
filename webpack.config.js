87895a32979d631f2ee21989780b74c856fffea5
d33pak-singh <singh.deepakk1992@gmail.com>
2017-06-04 02:31:09 +0530

    Few Final Tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,9 @@ module.exports = {
     jquery: 'jQuery'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
     new webpack.ProvidePlugin({
       '$': 'jquery',
       'jQuery': 'jquery'
